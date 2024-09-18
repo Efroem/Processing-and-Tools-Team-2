00a7891420311d27643dcb2b2f1014ca9a8fafeb
Efroem <134206024+Efroem@users.noreply.github.com>
2024-09-18 13:48:28 +0200

    Test

--- CargoHubRefactor/Program.cs
+++ CargoHubRefactor/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+// sofjdoasjdfoa
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
