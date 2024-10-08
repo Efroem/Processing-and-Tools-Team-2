11e6f58c4278eb0f153cee0cdaabad5731e68273
MayorMayonnaise <xanderbos04@hotmail.com>
2024-10-08 10:47:42 +0200

    added test files for each model

---
+++ CargoHub/api/tests/test_clients.py
@@ -0,0 +1,26 @@
+# Test file for all GET methods in the code
+
+# To run tests. run the following command
+# pytest api/tests/test_clients.py
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
+
+
+from api.models import orders
+from api.providers import data_provider
+
+data_provider.init()
+
+# print(data_provider.fetch_client_pool().get_clients())
+
+
+def test_get_clients():
+    clients = data_provider.fetch_client_pool().get_clients()
+    assert len(clients) >= 0
+
+
+
+
