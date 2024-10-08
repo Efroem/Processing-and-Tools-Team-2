0a90a853f6ed7ef595097f7bb43566278f787e87
MayorMayonnaise <xanderbos04@hotmail.com>
2024-10-08 14:29:55 +0200

    Added Get tests voor inventories en clients

--- CargoHub/api/tests/test_clients.py
+++ CargoHub/api/tests/test_clients.py
@@ -1,14 +1,16 @@
 # Test file for all GET methods in the code
 
 # To run tests. run the following command
-# pytest api/tests/test_clients.py
-
+# $env:PYTHONPATH="<root directory>" ; pytest api/tests/test_clients.py
+import pytest
+import requests
 import sys
 import os
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 
-
+from api.models import clients
 from api.models import orders
 from api.providers import data_provider
 
@@ -17,9 +19,100 @@ data_provider.init()
 # print(data_provider.fetch_client_pool().get_clients())
 
 
+@pytest.fixture
+def _data():
+    return [{'URL': 'http://localhost:3000/api/v1/'}, {"API_KEY": "a1b2c3d4e5"}]
+
+
+def test_get_clients_no_key_integration(_data):
+    url = _data[0]["URL"] + 'clients'
+    # params = {'id': 12}
+
+    # Send a GET request to the API
+    response = requests.get(url)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+
+    # Verify that the status code is 401 (Unauthorized)
+    assert status_code == 401
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+def test_get_clients_integration(_data):
+    url = _data[0]["URL"] + 'clients'
+    # params = {'id': 12}
+    header = _data[1]
+
+    # Send a GET request to the API
+    response = requests.get(url, headers=header)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+
+    # Verify that the status code is 200 (OK)
+    assert status_code == 200
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+def test_get_clients_by_id_no_key_integration(_data):
+    url = _data[0]["URL"] + 'clients/12'
+    # params = {'id': 12}
+
+    # Send a GET request to the API
+    response = requests.get(url)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+    # Verify that the status code is 401 (Unauthorized)
+    assert status_code == 401 ## and response_data["id"] == 12
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+def test_get_client_by_id_integration(_data):
+    url = _data[0]["URL"] + 'clients/12'
+    # params = {'id': 12}
+    header = _data[1]
+
+    # Send a GET request to the API
+    response = requests.get(url, headers=header)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    response_data = response.json()
+
+    # Verify that the status code is 200 (OK)
+    assert status_code == 200 and response_data["id"] == 12
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+
+
 def test_get_clients():
     clients = data_provider.fetch_client_pool().get_clients()
     assert len(clients) >= 0
+
+
+def test_get_client_by_id():
+    client = data_provider.fetch_client_pool().get_client(35)
+    assert client["id"] == 35
+
+
+
 
 
 
--- CargoHub/api/tests/test_inventories.py
+++ CargoHub/api/tests/test_inventories.py
@@ -5,6 +5,8 @@
 
 import sys
 import os
+import pytest
+import requests
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 
@@ -14,13 +16,101 @@ from api.providers import data_provider
 
 data_provider.init()
 
-# print(data_provider.fetch_client_pool().get_clients())
 
 
-# def test_get_clients():
-#     # data_provider.init()
-#     clients = data_provider.fetch_client_pool().get_clients()
-#     assert len(clients) >= 0
+@pytest.fixture
+def _data():
+    return [{'URL': 'http://localhost:3000/api/v1/'}, {"API_KEY": "a1b2c3d4e5"}]
+
+
+def test_get_inventories_no_key_integration(_data):
+    url = _data[0]["URL"] + 'inventories'
+    # params = {'id': 12}
+
+    # Send a GET request to the API
+    response = requests.get(url)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+
+    # Verify that the status code is 401 (Unauthorized)
+    assert status_code == 401
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+def test_get_inventories_integration(_data):
+    url = _data[0]["URL"] + 'inventories'
+    # params = {'id': 12}
+    header = _data[1]
+
+    # Send a GET request to the API
+    response = requests.get(url, headers=header)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+
+    # Verify that the status code is 200 (OK)
+    assert status_code == 200
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+def test_get_inventory_by_id_no_key_integration(_data):
+    url = _data[0]["URL"] + 'inventories/12'
+    # params = {'id': 12}
+
+    # Send a GET request to the API
+    response = requests.get(url)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    # response_data = response.json()
+    # Verify that the status code is 401 (Unauthorized)
+    assert status_code == 401 ## and response_data["id"] == 12
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+def test_get_inventory_by_id_integration(_data):
+    url = _data[0]["URL"] + 'inventories/12'
+    # params = {'id': 12}
+    header = _data[1]
+
+    # Send a GET request to the API
+    response = requests.get(url, headers=header)
+
+    # Get the status code and response data
+    status_code = response.status_code
+    response_data = response.json()
+
+    # Verify that the status code is 200 (OK)
+    assert status_code == 200 and response_data["id"] == 12
+
+    # Verify the response data
+    # assert response_data['id'] == 123
+    # assert response_data['name'] == 'John Smith'
+
+
+
+
+def test_get_inventories():
+    inventories = data_provider.fetch_inventory_pool().get_inventories()
+    assert len(inventories) >= 0
+
+
+def test_get_inventory_by_id():
+    inventory = data_provider.fetch_inventory_pool().get_inventory(35)
+    assert inventory["id"] == 35
+
+
 
 
 
