84ffe4d22e9b9a725e4fee193218ec5b67310f4f
Nolanimations <73882894+nolanimations@users.noreply.github.com>
2024-11-11 17:00:44 +0100

    update test

--- CargoHubRefactorTesting/DummyTest.cs
+++ CargoHubRefactorTesting/DummyTest.cs
@@ -6,23 +6,3 @@ using Microsoft.VisualStudio.TestPlatform.TestHost;
 using CargoHubRefactor;
 
 namespace CargoHubRefactorTesting;
-
-public class DummyTest : IClassFixture<WebApplicationFactory<CargoHubRefactor.Program>>{
-        private readonly HttpClient _client;
-        private readonly WebApplicationFactory<CargoHubRefactor.Program> _factory;
-
-    
-        public DummyTest(WebApplicationFactory<CargoHubRefactor.Program> factory)
-        {
-            _factory = factory;
-        }
-
-    [Fact]
-    public async Task GetDummyTest() {
-        var _client = _factory.CreateClient();
-        var response = await _client.GetAsync($"/dummy");
-        Assert.True(response.IsSuccessStatusCode);
-    }
-
-
-}
