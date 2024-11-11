03eaac41d82c82ca9d7d18ccb64ad6ab14f2ee69
MayorMayonnaise <xanderbos04@hotmail.com>
2024-11-11 11:59:29 +0100

    Fixed testing CargoHub and changed workflow file to run said tests

--- CargoHub/api/tests/test_locations.py
+++ CargoHub/api/tests/test_locations.py
@@ -8,6 +8,7 @@ import os
 import pytest
 import requests
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 
 
