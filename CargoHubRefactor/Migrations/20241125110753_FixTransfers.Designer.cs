1e2343a5e81b082e6a86bdb96e7b612f731bf92c
Nolanimations <73882894+nolanimations@users.noreply.github.com>
2024-11-25 12:16:17 +0100

    add transfers functionality

--- CargoHubRefactor/Migrations/20241125110753_FixTransfers.Designer.cs
+++ CargoHubRefactor/Migrations/20241125110753_FixTransfers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CargoHubRefactor.Migrations
 {
     [DbContext(typeof(CargoHubDbContext))]
-    partial class CargoHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241125110753_FixTransfers")]
+    partial class FixTransfers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
--- CargoHubRefactor/Program.cs
+++ CargoHubRefactor/Program.cs
@@ -15,7 +15,7 @@ namespace CargoHubRefactor
 
             builder.Services.AddSession(options => 
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(30);
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
 
                 options.Cookie.HttpOnly = true; 
                 options.Cookie.IsEssential = true; 
@@ -34,6 +34,7 @@ namespace CargoHubRefactor
             builder.Services.AddScoped<IItemLineService, ItemLineService>();
             builder.Services.AddScoped<IItemTypeService, ItemTypeService>();
             builder.Services.AddScoped<IItemService, ItemService>();
+            builder.Services.AddScoped<ITransferService, TransferService>();
             
             var app = builder.Build();
 
